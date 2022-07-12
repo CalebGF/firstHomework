92261248a042adcd9c284c7e9712c9e3a0eb1c2d
AzureAD\CarlosOrbegozo <carlos.orbegozo@securitasdirect.es>
2022-07-12 22:06:47 +0200

    added Chracter, warrior, wizard, party structure

--- src/cat/ironhack/wizard/Wizard.java
+++ src/cat/ironhack/wizard/Wizard.java
@@ -1,4 +1,32 @@
 package cat.ironhack.wizard;
 
-public class Wizard {
+import cat.ironhack.character.Character;
+
+import java.util.UUID;
+
+public class Wizard extends Character {
+    private int mana;
+    private int intelligence;
+
+    public Wizard(String name, int hp, boolean isAlive, int mana, int intelligence) {
+        super(name, hp, isAlive);
+        this.mana = mana;
+        this.intelligence = intelligence;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
 }
