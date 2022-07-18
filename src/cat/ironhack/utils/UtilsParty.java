a28281467f5af7bcffdb037c28c98843d26f3be4
AzureAD\CarlosOrbegozo <carlos.orbegozo@securitasdirect.es>
2022-07-18 15:28:10 +0200

    resolved conflicts

--- src/cat/ironhack/utils/UtilsParty.java
+++ src/cat/ironhack/utils/UtilsParty.java
@@ -12,9 +12,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import java.io.IOException;
-
 public class UtilsParty {
+
     public static Party generatePartyRandom() {
         return null;
     }
@@ -28,12 +27,15 @@ public class UtilsParty {
         for (int i = 0; i < Files.lines(path).count()-1; i++) {
             if (i > 0) {
                 String[] values = scannedFile.nextLine().split(","); //divides the current line i an Array of strings
-                switch (values[0].replaceAll("\"", "")) {
-                    case "wizard" -> characters.add(new Wizard(values[1], Integer.parseInt(values[2]), Boolean.parseBoolean(values[3]), Integer.parseInt(values[4]), Integer.parseInt(values[5])));
-                    case "warrior" -> characters.add(new Warrior(values[1], Integer.parseInt(values[2]), Boolean.parseBoolean(values[3]), Integer.parseInt(values[4]), Integer.parseInt(values[5])));
-                    default -> {
-                    }
-                    //aqui un print? con un newError y/o generar un character random?
+                switch (values[0].replaceAll("\"", "")){
+                    case "wizard":
+                        characters.add(new Wizard(values[1],Integer.parseInt(values[2]),Boolean.parseBoolean(values[3]),Integer.parseInt(values[4]),Integer.parseInt(values[5])));
+                        break;
+                    case "warrior":
+                        characters.add(new Warrior(values[1],Integer.parseInt(values[2]),Boolean.parseBoolean(values[3]),Integer.parseInt(values[4]),Integer.parseInt(values[5])));
+                        break;
+                    default:
+                        //aqui un print? con un newError y/o generar un character random?
                 }
             }
         }
