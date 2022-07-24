5220c6a323a7e688a9da71a0977e6c35e57d246f
pauGuarch <guarch.pau@gmail.com>
2022-07-24 21:38:51 +0200

    add battleScreen method and submethods

--- src/cat/ironhack/utils/UtilsMenu.java
+++ src/cat/ironhack/utils/UtilsMenu.java
@@ -1,4 +1,8 @@
 package cat.ironhack.utils;
+import cat.ironhack.battle.Battle;
+import cat.ironhack.party.Party;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,5 +14,51 @@ public class UtilsMenu {
 
     public static String readOption(String option) throws IOException {
         return Files.readString(Paths.get("src/resources/option/"+option+".txt"));
+    }
+
+    public static String refreshBattleScreen(String battleRound) throws IOException {
+        return Files.readString(Paths.get("src/resources/battleRound.txt"));
+    }
+
+    /**
+     *
+     * @param battle
+     * @throws IOException
+     */
+    public static void generateBattleTxt(Battle battle) throws IOException {
+        String battleHeader = "\n\n------------BATTLE PLAYGROUND------------\n";
+        String round = "Round :";
+        String chooseCharacters = "Choose the character that you want to use, and the opponent to attack:\n";
+        String graveyard = "------------GRAVEYARD------------\n";
+
+        FileWriter fileWriter = new FileWriter("src/resources/battleRound.txt", false);
+        fileWriter.write(battleHeader);
+        fileWriter.write(round+battle.getRound()+"\n");
+        fileWriter.write(chooseCharacters);
+
+        if(battle.getRound()%2==1) {
+            generateList(fileWriter, battle.getParty1(), battle.getParty2());
+        }else{
+            generateList(fileWriter, battle.getParty2(), battle.getParty1());
+        }
+        fileWriter.write(graveyard);
+        for (int i = 0; i < battle.getGraveyard().size(); i++) {
+            fileWriter.write(battle.getGraveyard().get(i).getName());
+        }
+        fileWriter.close();
+    }
+
+    private static void generateList(FileWriter fileWriter, Party party1, Party party2) throws IOException {
+        String attackers = "\nATTACKERS\n";
+        String defenders = "\nDEFENDERS\n";
+        fileWriter.write(attackers);
+        for (int i = 0; i < party1.getCharacters().size(); i++) {
+            fileWriter.write((i+1) + " " + party1.getCharacters().get(i).getName()+"\n");
+        }
+        fileWriter.write(defenders);
+        for (int i = 0; i < party2.getCharacters().size(); i++) {
+            fileWriter.write((i+1) + " " + party2.getCharacters().get(i).getName()+"\n");
+        }
+
     }
 }
