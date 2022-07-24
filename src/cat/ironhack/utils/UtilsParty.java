539917b31e876062b4af454c60fce32e70178c57
CalebGF <caleb2812@gmail.com>
2022-07-24 13:10:19 +0200

    GeneratePartyRandom created, UtilsRandom added

--- src/cat/ironhack/utils/UtilsParty.java
+++ src/cat/ironhack/utils/UtilsParty.java
@@ -2,24 +2,53 @@ package cat.ironhack.utils;
 
 import cat.ironhack.character.Character;
 import cat.ironhack.party.Party;
-import cat.ironhack.warrior.Warrior;
-import cat.ironhack.wizard.Wizard;
+import cat.ironhack.character.Warrior;
+import cat.ironhack.character.Wizard;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UtilsParty {
+    /**
+     *@param characters will store all the characters from the party, wizards and warriors
+     *@param names Have some names to assign randomly to the random chars
+     *@param randomNum will call the method from utilsRandom and generate a random number
+     */
 
+    static ArrayList<Character> characters = new ArrayList<Character>();
+    static ArrayList<String> names = new ArrayList<String>(Arrays.asList("Adams", "Baker", "Clark", "Davis", "Evans", "Frank", "Ghosh"));
+    private static UtilsRandom randomNum;
+
+    /**
+     * This method will generate random values given some limits
+     * @return  A full party with random characters
+     */
     public static Party generatePartyRandom() {
-        return null;
+        //To generate the partySize randomly
+        int partySize = randomNum.getRandomNum(2,15);
+        for (int i = 0; i < partySize; i++) {
+            String name = names.get(randomNum.getRandomNum(0, 5));
+            int healthPoints = randomNum.getRandomNum(50, 100);
+            int staminaOrMana = randomNum.getRandomNum(10,50);
+            int strengthOrIntelligence = randomNum.getRandomNum(1,50);
+            boolean isAlive = true;
+
+            //Having all the random values now we can randomly decide if it's going to be a Warrior or Wizard
+            if (randomNum.getRandomNum(0,1) == 0){
+                characters.add(new Wizard(name, healthPoints, isAlive, staminaOrMana, strengthOrIntelligence));
+            }else{
+                characters.add(new Warrior(name, healthPoints, isAlive, staminaOrMana, strengthOrIntelligence));
+            }
+        }
+        return new Party(characters);
     }
 
     public static Party generatePartyFromCSV(String csvfile) throws IOException {
-        ArrayList<Character> characters = new ArrayList<Character>();
         File file = new File(csvfile);
         Scanner scannedFile = new Scanner(file);
         Path path = Paths.get(csvfile);
---
+++ src/cat/ironhack/utils/UtilsRandom.java
@@ -0,0 +1,13 @@
+package cat.ironhack.utils;
+
+public class UtilsRandom {
+    /**
+     * Method used to get a random number given a min and a max
+     * @param min will get the minimal value
+     * @param max will get the maximal value
+     * @return a random number in the range given
+     */
+    public int getRandomNum(int min, int max) {
+        return (int) Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
