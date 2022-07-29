d4e780e966d8c7a348d8430d53b6f612246c516f
pauGuarch <guarch.pau@gmail.com>
2022-07-29 14:19:43 +0200

    refactor battlescreeen and roundmenu

--- src/cat/ironhack/utils/UtilsMenu.java
+++ src/cat/ironhack/utils/UtilsMenu.java
@@ -1,5 +1,6 @@
 package cat.ironhack.utils;
 import cat.ironhack.battle.Battle;
+import cat.ironhack.character.Character;
 import cat.ironhack.party.Party;
 
 import java.io.FileWriter;
@@ -19,32 +20,35 @@ public class UtilsMenu {
     public static String refreshBattleScreen(String battleRound) throws IOException {
         return Files.readString(Paths.get("src/resources/battleRound.txt"));
     }
-
+    //method to generate the menu shown after a fight between two characters is over
     public static void roundMenu(Battle battle, String id1, String id2) throws IOException {
-        String roundHeader = "--------------ROUND IS OVER------------\n";
-        FileWriter fileWriter = new FileWriter("src/resources/menu/roundMenu.txt", false);
+        String roundHeader = "-".repeat(17)+"ROUND IS OVER"+"-".repeat(17)+"\n";
+        FileWriter fileWriter = new FileWriter("src/resources/menu/roundScreen-options.txt", false);
         fileWriter.write(roundHeader);
         int player1Id = Integer.parseInt(id1);
         int player2Id = Integer.parseInt(id2);
-        if (battle.getParty1().getAliveCharacters().get(player1Id).isAlive()){
-            fileWriter.write(battle.getParty1().getAliveCharacters().get(player1Id).getName()+
-                    " killed "+battle.getParty2().getAliveCharacters().get(player1Id).getName());
-        }
+        Character player1 = battle.getParty1().getAliveCharacters().get(player1Id);//get the players by index received
+        Character player2 = battle.getParty1().getAliveCharacters().get(player2Id);
 
-        fileWriter.write(battle.getParty2().getAliveCharacters().get(player2Id).getName());
+        String killed = "====KILLED===>>";
+        if (player1.isAlive()){//if player 1 won the fight, we build the string showing him as winner, or otherwise.
+            fileWriter.write("         "+player1.getName().toUpperCase()+
+                    killed+player2.getName().toUpperCase()+"\n");
+            fileWriter.write("\nStats: "+player1);
+        }else{
+            fileWriter.write(player2.getName().toUpperCase()+
+                    " killed "+player1.getName().toUpperCase());
+            fileWriter.write("\nStats: "+player2);
+        }
         fileWriter.close();
     }
 
-    /**
-     *
-     * @param battle
-     * @throws IOException
-     */
+    //method to refresh the battle status and show current characters listed by party and dead characters
     public static void refreshBattleTxt(Battle battle) throws IOException {
-        String battleHeader = "\n\n------------BATTLE PLAYGROUND------------\n";
+        String battleHeader = "\n\n"+"-".repeat(12)+"BATTLE PLAYGROUND"+"-".repeat(12)+"\n";
         String round = "Round :";
         String chooseCharacters = "Choose the character that you want to use, and the opponent to attack:\n";
-        String graveyard = "------------GRAVEYARD------------\n";
+        String graveyard = "\n"+"-".repeat(61)+"GRAVEYARD"+"-".repeat(61)+"\n";
 
         FileWriter fileWriter = new FileWriter("src/resources/menu/battleRound.txt", false);
         fileWriter.write(battleHeader);
@@ -58,35 +62,35 @@ public class UtilsMenu {
         }
         fileWriter.write(graveyard);
         for (int i = 0; i < battle.getGraveyard().size(); i++) {
-            fileWriter.write(battle.getGraveyard().get(i).getName());
+            fileWriter.write("-"+battle.getGraveyard().get(i).getName()+"-");
         }
         fileWriter.close();
     }
-
+    //method to generate the list of each party's characters
     private static void generateList(FileWriter fileWriter, Party party1, Party party2) throws IOException {
-        String charactersHeader = "\nPARTY(1)----------------PARTY(2)\n";
+        String charactersHeader = "\n"+"-".repeat(23)+"PARTY(1)"+"-".repeat(68)+"PARTY(2)"+"-".repeat(23)+"\n";
 
         fileWriter.write(charactersHeader);
         for (int i = 0; i < Math.max(party1.getCharacters().size(), party2.getCharacters().size()); i++) {
             if(party2.getAliveCharacters().size()<i+1){ //getCharacters
                 int index = party1.getIndexFromCharacter(party1.getAliveCharacters().get(i));
-                fileWriter.write((index+1) + "-" + party1.getAliveCharacters().get(i).getName() + "\n");
+                fileWriter.write((index+1) + "-" + party1.getAliveCharacters().get(i).toString().concat("\n"));
             }else if(party1.getAliveCharacters().size()<i+1){
                 int index = party2.getIndexFromCharacter(party2.getAliveCharacters().get(i));
                 String str = "                        " +(index+1) +" "+
-                        party2.getAliveCharacters().get(i).getName() + "\n";
+                        party2.getAliveCharacters().get(i).toString().concat("\n");
                 fileWriter.write(str);
             }else{
                 int index = party1.getIndexFromCharacter(party1.getAliveCharacters().get(i));
                 StringBuilder str = new StringBuilder();
-                str.append(index + 1).append(" ").append(party1.getAliveCharacters().get(i).getName());
+                str.append(index + 1).append(" ").append(party1.getAliveCharacters().get(i).toString());
                 int j = 0;
                 while(j+party1.getAliveCharacters().get(i).getName().length()<22){
                     str.append(' ');
                     j++;
                 }
                 int index2 = party2.getIndexFromCharacter(party2.getAliveCharacters().get(i));
-                str.append((index2+1)+" "+party2.getAliveCharacters().get(i).getName()).append("\n");
+                str.append((index2+1)+" "+party2.getAliveCharacters().get(i).toString().concat("\n"));
                 fileWriter.write(str.toString());
             }
         }
